efb7d3ae1404f9535ab071f4622a3daaa76caa63
Matthieu Bourgain <aohzan@gmail.com>
2025-04-23 14:09:55 +0200

    fix: auth handle on API

--- custom_components/ipx800v5/request_views.py
+++ custom_components/ipx800v5/request_views.py
@@ -15,26 +15,23 @@ from .const import PUSH_USERNAME
 _LOGGER = logging.getLogger(__name__)
 
 
-def api_call_not_authorized(msg: str):
-    """Raise an API call not authorized."""
-    raise ApiCallNotAuthorized(msg)
-
-
 def check_api_auth(request, host, push_password) -> bool:
     """Check authentication on API call."""
-    _LOGGER.debug("Check API authentication from %s (expected %s)", request.remote, host)
+    _LOGGER.debug(
+        "Check API authentication from %s (expected %s)", request.remote, host
+    )
     try:
         if request.remote != host:
-            api_call_not_authorized("API call not coming from IPX800 IP.")
+            raise ApiCallNotAuthorized("API call not coming from IPX800 IP.")
         if "Authorization" not in request.headers:
-            api_call_not_authorized("API call no authentication provided.")
+            raise ApiCallNotAuthorized("API call no authentication provided.")
         header_auth = request.headers["Authorization"]
         split = header_auth.strip().split(" ")
         if len(split) != 2 or split[0].strip().lower() != "basic":
-            api_call_not_authorized("Malformed Authorization header")
+            raise ApiCallNotAuthorized("Malformed Authorization header")
         username, password = b64decode(split[1]).decode().split(":", 1)
         if username != PUSH_USERNAME or password != push_password:
-            api_call_not_authorized("API call authentication invalid.")
+            raise ApiCallNotAuthorized("API call authentication invalid.")
     except ApiCallNotAuthorized as err:
         _LOGGER.warning(err)
         return False
@@ -51,24 +48,38 @@ class IpxRequestView(HomeAssistantView):
         """Init the IPX view."""
         self.host = host
         self.password = password
-        self.url = "/api/%s/{entity_id}/{state}/" % slugify(ipx_name)
-        self.extra_urls = ["/api/ipx800v5/{entity_id}/{state}"]  # retrocompat
-        _LOGGER.debug("Dedicated push url for '%s': '%s'", ipx_name, self.url)
+        self.url = "/api/%s/{entity_id}/{state}" % slugify(ipx_name)
+        self.extra_urls = [
+            "/api/ipx800v5/{entity_id}/{state}"
+        ]  # retrocompat to remove in next major versions
+        _LOGGER.info("Dedicated push url for '%s': '%s'", ipx_name, self.url)
+        for extra_url in self.extra_urls:
+            _LOGGER.info(
+                "/!\\ Removed in next major version: legacy push refresh url for '%s': '%s'",
+                ipx_name,
+                extra_url,
+            )
         super().__init__()
 
     async def get(self, request, entity_id, state):
         """Respond to requests from the device."""
-        if check_api_auth(request, self.host, self.password):
-            _LOGGER.debug("State update pushed from IPX")
-            hass = request.app["hass"]
-            old_state = hass.states.get(entity_id)
-            _LOGGER.debug("Update %s to state %s", entity_id, state)
-            if old_state:
-                hass.states.async_set(entity_id, state, old_state.attributes)
-                return web.Response(status=HTTPStatus.OK, text="OK")
-            _LOGGER.warning("Entity not found for state updating: %s", entity_id)
-        _LOGGER.warning("Authentication for PUSH invalid")
-        return None
+        if not check_api_auth(request, self.host, self.password):
+            return web.Response(status=HTTPStatus.UNAUTHORIZED)
+        # To be removed in next major version
+        if "/api/ipx800v5/" in str(request.url) and "/api/ipx800v5/" not in self.url:
+            _LOGGER.warning(
+                "Legacy URL %s called, please update your IPX800 configuration",
+                str(request.url),
+            )
+        _LOGGER.debug("State update pushed from IPX")
+        hass = request.app["hass"]
+        old_state = hass.states.get(entity_id)
+        _LOGGER.debug("Update %s to state %s", entity_id, state)
+        if old_state:
+            hass.states.async_set(entity_id, state, old_state.attributes)
+            return web.Response(status=HTTPStatus.OK, text="OK")
+        _LOGGER.warning("Entity not found for state updating: %s", entity_id)
+        return web.Response(status=HTTPStatus.BAD_REQUEST)
 
 
 class IpxRequestDataView(HomeAssistantView):
@@ -83,33 +94,43 @@ class IpxRequestDataView(HomeAssistantView):
         self.password = password
         self.url = "/api/%s_data/{data}" % slugify(ipx_name)
         self.extra_urls = ["/api/ipx800v5_data/{data}"]  # retrocompat
-        _LOGGER.debug("Dedicated push data url for '%s': '%s'", ipx_name, self.url)
+        _LOGGER.info("Dedicated push data url for '%s': '%s'", ipx_name, self.url)
+        for extra_url in self.extra_urls:
+            _LOGGER.info(
+                "/!\\ Removed in next major version: legacy push refresh url for '%s': '%s'",
+                ipx_name,
+                extra_url,
+            )
         super().__init__()
 
     async def get(self, request, data):
         """Respond to requests from the device."""
-        if check_api_auth(request, self.host, self.password):
-            _LOGGER.debug("State update pushed from IPX")
-            hass = request.app["hass"]
-            entities_data = data.split("&")
-            for entity_data in entities_data:
-                entity_id = entity_data.split("=")[0]
-                state = (
-                    "on" if entity_data.split("=")[1] in ["1", "on", "true"] else "off"
-                )
+        if not check_api_auth(request, self.host, self.password):
+            return web.Response(status=HTTPStatus.UNAUTHORIZED)
+        # To be removed in next major version
+        if (
+            "/api/ipx800v5_data/" in str(request.url)
+            and "/api/ipx800v5_data/" not in self.url
+        ):
+            _LOGGER.warning(
+                "Legacy URL %s called, please update your IPX800 configuration",
+                str(request.url),
+            )
+        _LOGGER.debug("State update pushed from IPX")
+        hass = request.app["hass"]
+        entities_data = data.split("&")
+        for entity_data in entities_data:
+            entity_id = entity_data.split("=")[0]
+            state = "on" if entity_data.split("=")[1] in ["1", "on", "true"] else "off"
 
-                old_state = hass.states.get(entity_id)
-                _LOGGER.debug("Update %s to state %s", entity_id, state)
-                if old_state:
-                    hass.states.async_set(entity_id, state, old_state.attributes)
-                else:
-                    _LOGGER.warning(
-                        "Entity not found for state updating: %s", entity_id
-                    )
+            old_state = hass.states.get(entity_id)
+            _LOGGER.debug("Update %s to state %s", entity_id, state)
+            if old_state:
+                hass.states.async_set(entity_id, state, old_state.attributes)
+            else:
+                _LOGGER.warning("Entity not found for state updating: %s", entity_id)
 
-            return web.Response(status=HTTPStatus.OK, text="OK")
-        _LOGGER.warning("Authentication for PUSH invalid")
-        return None
+        return web.Response(status=HTTPStatus.OK, text="OK")
 
 
 class IpxRequestRefreshView(HomeAssistantView):
@@ -134,17 +155,31 @@ class IpxRequestRefreshView(HomeAssistantView):
             "/api/ipx800v5_refresh",  # retrocompat
             "/api/ipx800v5_refresh/{data}",  # retrocompat
         ]
-        _LOGGER.debug("Dedicated push refresh url for '%s': '%s'", ipx_name, self.url)
+        _LOGGER.info("Dedicated push refresh url for '%s': '%s'", ipx_name, self.url)
+        for extra_url in self.extra_urls:
+            _LOGGER.info(
+                "/!\\ Removed in next major version: legacy push refresh url for '%s': '%s'",
+                ipx_name,
+                extra_url,
+            )
         super().__init__()
 
-    async def get(self, request, data):
+    async def get(self, request):
         """Respond to requests from the device."""
-        if check_api_auth(request, self.host, self.password):
-            _LOGGER.debug("Update asked from IPX PUSH")
-            await self.coordinator.async_request_refresh()
-            return web.Response(status=HTTPStatus.OK, text="OK")
-        _LOGGER.warning("Authentication for PUSH invalid")
-        return None
+        if not check_api_auth(request, self.host, self.password):
+            return web.Response(status=HTTPStatus.UNAUTHORIZED)
+        # To be removed in next major version
+        if (
+            "/api/ipx800v5_refresh" in str(request.url)
+            and "/api/ipx800v5_refresh" not in self.url
+        ):
+            _LOGGER.warning(
+                "Legacy URL %s called, please update your IPX800 configuration",
+                str(request.url),
+            )
+        _LOGGER.debug("Update asked from IPX PUSH")
+        await self.coordinator.async_request_refresh()
+        return web.Response(status=HTTPStatus.OK, text="OK")
 
 
 class ApiCallNotAuthorized(BaseException):
